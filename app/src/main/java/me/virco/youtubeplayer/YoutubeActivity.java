38c3850d9285bda03006a7ec7bd5469b1cf43569
virco <dev.virco@gmail.com>
2017-04-26 21:55:53 -0400

    Initial commit

---
+++ app/src/main/java/me/virco/youtubeplayer/YoutubeActivity.java
@@ -0,0 +1,38 @@
+package me.virco.youtubeplayer;
+
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_youtube);
+//        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+        Button button1 = new Button(this);
+        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
+        button1.setText("Button added");
+        layout.addView(button1);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+}
